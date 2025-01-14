3b568847c3eb2284b5216b6312d27abd88f853e5
Massimiliano Gilli <massimiliano.gilli@exida-eng.com>
2025-01-14 13:40:06 +0100

    v0.4.4

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   themeConfig: {
     // logo: 'TODO', https://github.com/vuejs/vitepress/issues/1401
-    appVersion: '0.4.3',
+    appVersion: '0.4.4',
     github: {
       releasesUrl: 'https://github.com/maxdd/Exiled-Exchange-2/releases'
     },
